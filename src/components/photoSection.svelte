55ae63ad1a133a78b65077ff4d4d8492075a765b
ImJustinTIme <49542450+ImJustinTIme@users.noreply.github.com>
2023-10-15 20:38:14 -0500

    add button to make it more obvious there are links

--- src/components/photoSection.svelte
+++ src/components/photoSection.svelte
@@ -1,66 +1,36 @@
 <script>
-  import gsap from 'gsap';
-  import {lazyLoad} from '@utils/lazyLoad.js';
+  import gsap from "gsap";
+  import { lazyLoad } from "@utils/lazyLoad.js";
 
   export let photos;
-  let { names, description, title, id, url} = photos;
+  let { names, description, title, id, url } = photos;
 
   const hoverOver = (event) => {
-    let [, span1, span2] = event.target.lastChild.children;
     gsap.to(event.target, {
-      boxShadow: '0 3px 10px rgb(0 0 0 / 0.2)'
+      boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)",
     });
-
-    gsap.to(span1, {
-      width: '200px',
-      opacity: 1,
-      duration: 2,
-      ease: 'ease-in'
-    });
-
-    gsap.to(span2, {
-      width: '80px',
-      opacity: 1,
-      duration: 1.4,
-      ease: 'ease-in'
-    });
-  }
+  };
 
   const hoverAway = (event) => {
-    let [, span1, span2] = event.target.lastChild.children;
     gsap.to(event.target, {
-    boxShadow: 'none'
-  });
-  gsap.to(span1, {
-    width: 0,
-    opacity: 0,
-    duration: 1,
-    ease: 'ease-in'
-  });
-
-  gsap.to(span2,{
-    width: 0,
-    opacity: 0,
-    duration: 1,
-    ease: 'ease-in'
-  });
+      boxShadow: "none",
+    });
   };
 
   const onLoad = () => {
-    gsap.to('.photo-section'  , {
-      opacity: 1, 
+    gsap.to(".photo-section", {
+      opacity: 1,
       y: 0,
       duration: 1.4,
-      ease: 'ease-in'
-  });
-  }
+      ease: "ease-in",
+    });
+  };
 </script>
 
-<a 
+<a
   on:mouseenter={hoverOver}
   on:mouseleave={hoverAway}
-  href={url}
-  {id} 
+  {id}
   class={`photo-section`}
 >
   {#if title}
@@ -68,12 +38,16 @@
   {/if}
   <div class="photo-grid" let:onLoad>
     {#each names as photoName}
-      <img on:load={onLoad} use:lazyLoad={photoName} alt={photoName}  class={`photo-${names.length}`}/>
+      <img
+        on:load={onLoad}
+        use:lazyLoad={photoName}
+        alt={photoName}
+        class={`photo-${names.length}`}
+      />
     {/each}
   </div>
   <div class="photo-subtext-section">
     <div>{description}</div>
-    <span class='line'/>
-    <span class='line-2'/>
+    <a href={url} class='link-button'>Purchase Full Image</a>
   </div>
 </a>
