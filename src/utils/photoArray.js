c6bfe327a92bbdad7ded9cb9bb17534527d0be14
ImJustinTIme <49542450+ImJustinTIme@users.noreply.github.com>
2023-11-13 14:31:13 -0600

    convert all the things to load the page faster

--- src/utils/photoArray.js
+++ src/utils/photoArray.js
@@ -1,22 +1,22 @@
 export const photos = [
     {
       id: "photos",
-      names: ["Black-capped Chickadee-2.jpg"],
+      names: ["Black-capped Chickadee.webp"],
       url: 'https://ko-fi.com/s/f86e18dd03',
       description:
         "Photo of black-capped chickadee taken spring 2023 in Spokane.",
     },
     {
-      names: ["Hoco-1.jpg", "Rock Face.jpg"],
+      names: ["Hoco-1.webp", "Rock Face.webp"],
       url: 'https://ko-fi.com/s/5bc3593be3',
       description:
         "Rock structure photos taken around the Spokane river, winter 2023.",
     },
     {
       names: [
-        "Subtle Anarchy 2.jpg",
-        "Subtle Anarchy-1.jpg",
-        "Subtle Messages.jpg",
+        "Subtle Anarchy 2.webp",
+        "Subtle Anarchy-1.webp",
+        "Subtle Messages.webp",
       ],
       url: 'https://ko-fi.com/s/5bc3593be3',
       description:
@@ -24,53 +24,53 @@ export const photos = [
     },
     {
       names: [
-        "Goose Feet B&W.jpg", 
-        "Goose Bay Watch.jpg"
+        "Goose Feet B&W.webp", 
+        "Goose Bay Watch.webp"
       ],
       url: 'https://ko-fi.com/s/588e7dc0f0',
       description: "I had some geese come right up to me to get the their picture taken.",
     },
     {
         names: [
-          "Goose Head-1.jpg", 
-          "Goose Smerk-1.jpg"
+          "Goose Head-1.webp", 
+          "Goose Smerk-1.webp"
         ],
         url: 'https://ko-fi.com/s/588e7dc0f0',
         description: "Some more close ups of the geese.",
       },
     {
       names: [
-        "Foggy Day In Spokane.jpg"
+        "Foggy Day In Spokane.webp"
       ],
       url: 'https://ko-fi.com/s/de2c7b619f',
       description: "Taken shortly after orignally getting the new camera. I really like the difference in leaves in the two trees.",
     },
     {
-        names: ["Ultrawide Chickadee.jpg"],
+        names: ["Ultrawide Chickadee.webp"],
         url: 'https://ko-fi.com/s/d87332cd02',
         description:
           "Photo of black-capped chickadee taken spring 2023 in Spokane in wide ratio.",
       },
     {
-        names: ["Walla Walla.jpg"],
+        names: ["Walla Walla.webp"],
         url: 'https://ko-fi.com/s/09d62cf6b7',
         description:
             "Taken near Amani Cellars Walla Walla, Washington.",
     },
     {
-        names: ["Nut Hatch-1.jpg", "Nut Hatch Lg-1.jpg"],
+        names: ["Nut Hatch-1.webp", "Nut Hatch Lg-1.webp"],
         url: 'https://ko-fi.com/s/d87332cd02',
         description:
           "Photos of some pygmy nuthatches found near the centennial trail.",
       },
       {
-        names: ["Hickory Hills Tree-1.jpg"],
+        names: ["Hickory Hills Tree-1.webp"],
         url: 'https://ko-fi.com/s/86b8f0da6b',
         description:
           "Photo taken fall 2023 at Hickory Hills",
       },
       {
-        names: ["Hickory Hills Tree 2.jpg", "Spike Tree-1.jpg"],
+        names: ["Hickory Hills Tree 2.webp", "Spike Tree-1.webp"],
         url: 'https://ko-fi.com/s/86b8f0da6b',
         description:
           "Photo taken fall 2023 at Hickory Hills",
@@ -79,16 +79,16 @@ export const photos = [
       title: "Digital Art",
       id: "digital-art",
       url: 'https://ko-fi.com/justinintime',
-      names: ["Stripes-2.PNG", "Stripes.PNG"],
+      names: ["Stripes-2.webp", "Stripes.webp"],
       description: "Some ipad wallpapers I made For personal use in Procreate.",
     },
     {
-      names: ["Fall Feelings 1080p.png"],
+      names: ["Fall Feelings 1080p.webp"],
       url: 'https://ko-fi.com/s/fdbde88901',
       description: "'Fall Feelings' made on Procreate"
     },
     {
-      names: ["Just a little greener small.png", "Just a little greener var.png"],
+      names: ["Just a little greener small.webp", "Just a little greener var.webp"],
       url: 'https://ko-fi.com/s/20940867ec',
       description: "'Just a Little Geener' made in Adobe Illustrator"
     }
