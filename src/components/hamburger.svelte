2ef06be6b94c691c8ab97d443f92984ab5b9943e
Justin kuennen <codejustin@proton.me>
2024-12-23 09:46:23 -0600

    try again with update component

--- src/components/hamburger.svelte
+++ src/components/hamburger.svelte
@@ -1,15 +1,14 @@
 <script>
     let { menuOpen, makeActive } = $props();
-
-    
 </script>
 
 <button
-    class={`hamburger hamburger--slider ${menuOpen ? 'is-active' : ''}`}
-    onclick={makeActive} 
+    class={`hamburger hamburger--slider ${menuOpen ? "is-active" : ""}`}
+    aria-label={"menu"}
+    onclick={makeActive}
     type="button"
 >
     <span class="hamburger-box">
-      <span class="hamburger-inner"></span>
+        <span class="hamburger-inner"></span>
     </span>
-</button>  
+</button>
