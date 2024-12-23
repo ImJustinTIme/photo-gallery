1e7a5fe119b2c8e07f5d95c9ee2891fecc8353c8
Justin kuennen <codejustin@proton.me>
2024-12-23 10:56:13 -0600

    convert

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': path.resolve(__dirname, './src/utils')
+    },
+  },
+
+  plugins: [
+    svelte({
+      /* plugin options */
+    })
+  ]
+});
